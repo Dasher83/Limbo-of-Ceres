1bb2ab5f8a205ac5c1a605e3da9e67d3fd62177c
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-11-13 12:29:52 -0300

    [WIP] Flicker effect without safe reset

--- Assets/Scripts/PlayerScripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerScripts/PlayerDamage.cs
@@ -1,4 +1,5 @@
 using QuarkAcademyJam1Team1.Scripts.Shared.Interfaces;
+using QuarkAcademyJam1Team1.Scripts.TimeScripts;
 using UnityEngine;
 
 namespace QuarkAcademyJam1Team1.Scripts.PlayerScritps
@@ -6,15 +7,36 @@ namespace QuarkAcademyJam1Team1.Scripts.PlayerScritps
     public class PlayerDamage : MonoBehaviour, IDamageable
     {
         private IDamageable damageable;
+        private ResettableTimer flickerTestTimer;
+        private PlayerFlicker playerFlicker;
 
         private void Start()
         {
             damageable = gameObject.GetComponent<PlayerDataContainer>().PlayerData;
+            playerFlicker = gameObject.GetComponent<PlayerFlicker>();
+        }
+
+        private void Update()
+        {
+            if (flickerTestTimer == null) return;
+            flickerTestTimer.Countdown(Time.deltaTime);
+            if (flickerTestTimer.OutOfTime)
+            {
+                playerFlicker.StopFlickering();
+                flickerTestTimer.Reset();
+            }
         }
 
         public void ReceiveDamage(int damage)
         {
+            if (playerFlicker.IsFlickering) return;
+
             damageable.ReceiveDamage(damage);
+            playerFlicker.StartFlickering();
+            if (flickerTestTimer == null)
+            {
+                flickerTestTimer = new ResettableTimer(5f);
+            }
         }
     }
 }
