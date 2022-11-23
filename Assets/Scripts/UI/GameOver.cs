bce88dbfd6550b16c6c37b199a1e49a2b19618be
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-11-22 22:47:53 -0300

    static english translation | next: multilenguage support

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -36,7 +36,7 @@ namespace QuarkAcademyJam1Team1.Scripts.UI
 
         public void StartGameOver()
         {
-            scoreText.text =  "TU PUNTUACION" + "\n" + playerData.CurrentMeters.ToString("0") + "M";
+            scoreText.text =  "SCORE" + "\n" + playerData.CurrentMeters.ToString("0") + "M";
             currentScore = (int)playerData.CurrentMeters;
             gameObject.SetActive(true);
         }
