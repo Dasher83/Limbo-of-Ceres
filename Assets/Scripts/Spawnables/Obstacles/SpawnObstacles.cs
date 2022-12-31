884985f7700e33a7fd5a9bda5caf75c330e7d6da
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-12-31 17:01:30 -0300

    Make obstacles use the new pooling system

--- Assets/Scripts/Spawnables/Obstacles/SpawnObstacles.cs
+++ Assets/Scripts/Spawnables/Obstacles/SpawnObstacles.cs
@@ -1,5 +1,5 @@
 using LimboOfCeres.Scripts.Shared;
-using LimboOfCeres.Scripts.Shared.ScriptableObjectsDefinitions;
+using LimboOfCeres.Scripts.Spawnables.Shared;
 using LimboOfCeres.Scripts.TimeScripts;
 using LimboOfCeres.Scripts.Utils;
 using System.Collections.Generic;
@@ -7,93 +7,30 @@ using UnityEngine;
 
 namespace LimboOfCeres.Scripts.Spawnables.Obstacles
 {
-    public class SpawnObstacles : MonoBehaviour
+    public class SpawnObstacles : ContinuousObjectSpawner
     {
-        [SerializeField]
-        private ObstacleDataScriptableObject obstacleData;
         [SerializeField]
         private SpriteRenderer floorSpriteRenderer;
         [SerializeField]
         private SpriteRenderer ceilingSpriteRenderer;
         [SerializeField]
-        private GameObject prefabToSpawn;
-        private ResettableTimer spawnTimer;
-        private Vector3 newPosition;
-        private List<GameObject> pool;
-        private GameObject newlyCreatedSpwanable;
-        private GameObject nextToBeSpawn;
-        [SerializeField]
         private List<Sprite> sprites;
+        private Vector3 newPosition;
 
-        private void Start()
+        protected override void Start()
         {
+            base.Start();
             newPosition = Vector3.zero;
-            spawnTimer = new ResettableTimer(time: Random.Range(obstacleData.MinimumRespawnTime, obstacleData.MaximumRespawnTime));
-            pool = new List<GameObject>();
-            newlyCreatedSpwanable = null;
         }
 
-        private void Update()
-        {
-            spawnTimer.Countdown(Time.deltaTime);
-
-            if (!spawnTimer.OutOfTime)
-            {
-                return;
-            }
-
-            Spawn();
-            nextToBeSpawn = null;
-            spawnTimer.Reset(time: Random.Range(obstacleData.MinimumRespawnTime, obstacleData.MaximumRespawnTime));
-        }
-
-        private void Spawn()
-        {
-            nextToBeSpawn = InactiveSpawnable;
-            if (nextToBeSpawn == null)
-            {
-                nextToBeSpawn = CreateNewSpawnable();
-                newlyCreatedSpwanable = null;
-            }
-            else
-            {
-                nextToBeSpawn.SetActive(true);
-            }
-
-            RePositionSpawnable();
-        }
-
-        private GameObject InactiveSpawnable
-        {
-            get
-            {
-                foreach (GameObject spawnable in pool)
-                {
-                    if (!spawnable.activeSelf)
-                    {
-                        return spawnable;
-                    }
-                }
-                return null;
-            }
-        }
-
-        private GameObject CreateNewSpawnable()
-        {
-            newlyCreatedSpwanable = Instantiate(prefabToSpawn, gameObject.transform);
-            InitializeNewSpawnable();
-            pool.Add(newlyCreatedSpwanable);
-            return newlyCreatedSpwanable;
-        }
-
-        private void InitializeNewSpawnable()
+        protected override void InitializeNewSpawnable()
         {
             newlyCreatedSpwanable.GetComponent<SpriteRenderer>().sprite = RandomSprite;
         }
 
         private Sprite RandomSprite => sprites[Random.Range(0, sprites.Count)];
 
-        private void RePositionSpawnable()
+        protected override void PositionSpawnable()
         {
             newPosition.x = CameraUtils.OrthographicBounds.max.x + nextToBeSpawn.GetComponent<SpriteRenderer>().bounds.size.x;
             nextToBeSpawn.GetComponent<Rigidbody2D>().gravityScale = Constants.Obstacles.DefaultGravityScale;
