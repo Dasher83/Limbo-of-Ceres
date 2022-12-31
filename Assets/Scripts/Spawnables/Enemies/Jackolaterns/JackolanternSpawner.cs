b863fd558c436b8df3ae7a20f958c79e5a44454d
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-12-30 21:30:22 -0300

    [Adds abstract ObjectSpawner] & [Makes JackolanternSpawner a subclass of ObjectSpawner]

---
+++ Assets/Scripts/Spawnables/Enemies/Jackolaterns/JackolanternSpawner.cs
@@ -0,0 +1,61 @@
+using LimboOfCeres.Scripts.Shared;
+using LimboOfCeres.Scripts.Utils;
+using UnityEngine;
+using LimboOfCeres.Scripts.Spawnables.Enemies.Shared;
+using LimboOfCeres.Scripts.PlayerScritps;
+using LimboOfCeres.Scripts.Spawnables.Shared;
+
+namespace LimboOfCeres.Scripts.Spawnables.Enemies.Jackolanterns
+{
+    public class JackolanternSpawner : ObjectSpawner
+    {
+        [SerializeField]
+        private SpawnPumpkinBullets pumpkinBulletsSpawner;
+        private Transform target = null;
+        private SpriteRenderer floorSpriteRenderer = null;
+        private SpriteRenderer ceilingSpriteRenderer = null;
+        private Vector3 newPosition;
+        private PlayerRespawnSafely playerRespawnSafely = null;
+
+        protected override void Start()
+        {
+            base.Start();
+            GameObject player = GameObject.FindGameObjectWithTag(Constants.Tags.Player);
+            target = player.transform;
+            playerRespawnSafely = player.GetComponent<PlayerRespawnSafely>();
+            floorSpriteRenderer = GameObject.Find(Constants.GameObjects.MainFloor).GetComponent<SpriteRenderer>();
+            ceilingSpriteRenderer = GameObject.Find(Constants.GameObjects.MainCeiling).GetComponent<SpriteRenderer>();
+            newPosition = Vector3.zero;
+        }
+
+        protected override void InitializeNewSpawnable()
+        {
+            this.newlyCreatedSpwanable.GetComponent<ShootPumpkin>().PumpkinBulletSpawner = pumpkinBulletsSpawner;
+            this.newlyCreatedSpwanable.GetComponent<ShootPumpkin>().PlayerRespawnSafely = playerRespawnSafely;
+            this.newlyCreatedSpwanable.GetComponent<FaceTarget>().LockedOnTarget = target;
+            this.newlyCreatedSpwanable.GetComponent<ShootPumpkin>().LockedOnTarget = target;
+        }
+
+        protected override void RePositionSpawnable()
+        {
+            newPosition.x = CameraUtils.OrthographicBounds.max.x + this.nextToBeSpawn.GetComponent<SpriteRenderer>().bounds.size.x;
+            this.nextToBeSpawn.GetComponent<Rigidbody2D>().gravityScale = Constants.Enemies.Jackolanterns.DefaultGravityScale;
+
+            if (Random.value >= Constants.Enemies.Jackolanterns.CeilingSpawnProbability)
+            {
+                newPosition.y = CameraUtils.OrthographicBounds.max.y - this.nextToBeSpawn.GetComponent<SpriteRenderer>().bounds.size.y;
+                newPosition.y -= ceilingSpriteRenderer.bounds.size.y;
+                this.nextToBeSpawn.GetComponent<SpriteRenderer>().flipY = true;
+                this.nextToBeSpawn.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            }
+            else
+            {
+                newPosition.y = CameraUtils.OrthographicBounds.min.y + this.nextToBeSpawn.GetComponent<SpriteRenderer>().bounds.size.y;
+                newPosition.y += floorSpriteRenderer.bounds.size.y;
+                this.nextToBeSpawn.GetComponent<SpriteRenderer>().flipY = false;
+            }
+
+            this.nextToBeSpawn.transform.position = newPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawnables/Shared/ObjectSpawner.cs
@@ -0,0 +1,86 @@
+using LimboOfCeres.Scripts.Shared.ScriptableObjectsDefinitions;
+using LimboOfCeres.Scripts.TimeScripts;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LimboOfCeres.Scripts.Spawnables.Shared
+{
+    public abstract class ObjectSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject prefabToSpawn;
+        [SerializeField]
+        private SpawningDataScriptable spawningData;
+
+        private List<GameObject> pool;
+        private ResettableTimer spawnTimer;
+        protected GameObject nextToBeSpawn;
+        protected GameObject newlyCreatedSpwanable;
+
+        protected virtual void Start()
+        {
+            pool = new List<GameObject>();
+            spawnTimer = new ResettableTimer(time: Random.Range(spawningData.MinimumRespawnTime, spawningData.MaximumRespawnTime));
+            nextToBeSpawn = null;
+            newlyCreatedSpwanable = null;
+        }
+
+        private void Update()
+        {
+            spawnTimer.Countdown(Time.deltaTime);
+
+            if (!spawnTimer.OutOfTime)
+            {
+                return;
+            }
+
+            Spawn();
+            nextToBeSpawn = null;
+            spawnTimer.Reset(time: Random.Range(spawningData.MinimumRespawnTime, spawningData.MaximumRespawnTime));
+        }
+
+        private void Spawn()
+        {
+            nextToBeSpawn = InactiveSpawnable;
+            
+            if (nextToBeSpawn == null)
+            {
+                nextToBeSpawn = CreateNewSpawnable();
+                newlyCreatedSpwanable = null;
+            }
+            else
+            {
+                nextToBeSpawn.SetActive(true);
+            }
+
+            RePositionSpawnable();
+        }
+
+        private GameObject InactiveSpawnable
+        {
+            get
+            {
+                foreach (GameObject spawnable in pool)
+                {
+                    if (!spawnable.activeSelf)
+                    {
+                        return spawnable;
+                    }
+                }
+                return null;
+            }
+        }
+
+        private GameObject CreateNewSpawnable()
+        {
+            newlyCreatedSpwanable = Instantiate(prefabToSpawn, gameObject.transform);
+            InitializeNewSpawnable();
+            pool.Add(newlyCreatedSpwanable);
+            return newlyCreatedSpwanable;
+        }
+
+        protected abstract void InitializeNewSpawnable();
+
+        protected abstract void RePositionSpawnable();
+    }
+}
